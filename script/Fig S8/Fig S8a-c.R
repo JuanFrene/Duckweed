2a2ad0514cca7f31341ecc4a503a29d0e1db8a39
JuanFrene <37119809+JuanFrene@users.noreply.github.com>
2026-04-22 09:04:10 -0300

    Add files via upload

---
+++ script/Fig S8/Fig S8a-c.R
@@ -0,0 +1,52 @@
+library(devtools)
+install_github("Russel88/MicEco")
+library(MicEco)
+library(phyloseq)
+library(microbiome)
+
+setwd("C:/Users/juanp/Google Drive/labs/Nottingham/Duckweed/16S Exp1/")
+
+# Taxonomy from dada
+taxa <- readRDS("G:/My Drive/labs/Nottingham/Duckweed/16S Exp1/tax_final250_220.rds")
+# check your taxonomic classifications #
+taxa.print<- taxa
+rownames(taxa.print)
+head(taxa.print)
+
+###Metadata
+meta <- read.table("Metadata.txt", header = TRUE, row.names = 1)
+
+###ASV table from dada
+asv.table<- readRDS("G:/My Drive/labs/Nottingham/Duckweed/16S Exp1/seqtab_final250_220.rds")
+asv.table2<- otu_table(asv.table, taxa_are_rows=FALSE)
+
+# 13. Make a Phyloseq object ####
+ps.1 <- phyloseq(asv.table2, tax_table(taxa), sample_data(meta))
+
+
+#Phyloseq selection
+ps.2 = subset_taxa(ps.1, Kingdom == "Bacteria" | Kingdom == "Archaea")
+ps.pruned <- prune_samples(sample_sums(ps.2)>=2, ps.2)
+ps.perc <- transform_sample_counts(ps.pruned, function(x) x / sum(x)) 
+ordered(sample_sums(ps.pruned))
+
+#Delete the blank
+ps.3 = subset_samples(ps.pruned, Compartment !=  "BLANK")
+taxa_names(ps.3) <- paste0("ASV", seq(ntaxa(ps.3)))
+
+###Core microbiome by species.
+##ps.3.9243
+ps.3.perc <- transform_sample_counts(ps.3, function(x) x / sum(x)) 
+ps.3.core <- core(ps.3.perc, detection = 1/1000, prevalence = 10/100)
+
+ps.3.R.W = subset_samples(ps.3, Compartment !=  "Front")
+ps.3.R.F = subset_samples(ps.3, Compartment !=  "Water")
+ps.3.W.F = subset_samples(ps.3, Compartment !=  "Root")
+
+ps.3@sam_data
+#
+saveRDS(ps.3.W.F, "G:/My Drive/labs/Nottingham/Duckweed/Figuras paper/Clean data/Fig S4/Fig S4g/seqtab.rds")
+seqtab <- readRDS("G:/My Drive/labs/Nottingham/Duckweed/Figuras paper/Clean data/Fig S4/Fig S4 e-g/seqtab.rds")
+
+#Diagram venn based on the core
+ps_venn(ps.3.core, group = "Compartment", fill = c("red", "blue"), quantities = list(type=c("counts")))
